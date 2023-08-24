f42b9b3930e505eed3663e30d45f83dfca58368c
Little Grey <51894967+djc-Sherlock@users.noreply.github.com>
2023-08-24 15:40:17 +0800

    Update xinzhai.js

--- xinzhai.js
+++ xinzhai.js
@@ -1,37 +1,35 @@
-const axios = require('axios');
+const { default: axios } = require('axios');
 const notify = require('./sendNotify'); // 这里是 node（青龙属于node环境）通知相关的
 const Notify = 1; //0为关闭通知，1为打开通知,默认为1
 const bondList = [];
-function checkTodayBond() {
+
+// 第一个函数：获取今天日期，并使用axios请求接口，筛选出符合条件的数据
+async function getBondList() {
 	try {
-		// 获取今天的日期
-		const today = new Date().toISOString().split('T')[0];
-		// 请求接口
-		axios
-			.get('https://www.jisilu.cn/data/cbnew/pre_list/')
-			.then(response => {
-				// 遍历数据
-				response.data.rows.forEach(row => {
-					const applyDate = row.cell.apply_date;
-					// 检查apply_date是否与今天相同
-					if (applyDate === '2023-08-23') {
-						bondList.push(row.cell.bond_nm);
-					}
-				});
-				console.log('总共有 ' + bondList.length + ' 条数据'); // 打印符合条件的数据数量
-				console.log('新债是: ' + bondList.join(', ')); // 打印符合条件的数据内容
-				msg = `今日可申购{bondList.length}只新债\n\n` + '分别是' + bondList.join(', ');
-				return msg;
-			})
-			.catch(error => {
-				console.error('请求接口时发生错误：', error);
-			});
+		const today = new Date().toISOString().split('T')[0]; // 获取今天日期
+
+		const response = await axios.get('https://www.jisilu.cn/data/cbnew/pre_list/'); // 发起请求
+		const data = response.data;
+
+		data.rows.forEach(row => {
+			if (row.cell.apply_date == today) {
+				bondList.push(row.cell.bond_nm); // 将符合条件的bond_nm加入到bondList数组中
+			}
+			else{
+				console.log('今天没有可转债需要申购'); 
+			}
+		});
+
+		console.log('总共有 ' + bondList.length + ' 只新债\n\n'); // 打印符合条件的数据数量
+		console.log('新债是: ' + bondList.join(', ')) + '\n\n'; // 打印符合条件的数据内容
+		msg = '🔔今日可申购 ' + bondList.length + ' 只新债\n\n' + '🔔' + bondList.join(', ');
+		await SendMsg(msg);
 	} catch (error) {
-		console.error('发生错误:', error);
-		return 0; // 返回默认值
+		console.log(error);
 	}
 }
-function SendMsg(message) {
+
+async function SendMsg(message) {
 	if (!message) return;
 
 	if (Notify > 0) {
@@ -41,4 +39,4 @@ function SendMsg(message) {
 		return console.log('不发送消息提醒');
 	}
 }
-SendMsg(checkTodayBond());
+getBondList();
